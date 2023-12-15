a795aff8ca4f9d45dfdac31d8975e164b498e6d9
Fatem-Zahra Abid <abid.fatem@hotmail.fr>
2023-12-15 14:47:20 +0100

    probleme à lancer react, voire conv chatgpt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from './Component/Footer';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Footer/>
+            </div>
+        )
+    }
+}
